5ccdf5c705a818bbfd0786fae1b0883b65331949
Joachim Metz <joachim.metz@gmail.com>
2016-09-05 07:08:26 +0200

    Worked on tests.

--- tests/vshadow_test_get_version.c
+++ tests/vshadow_test_get_version.c
@@ -20,7 +20,6 @@
  */
 
 #include <common.h>
-#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
@@ -28,6 +27,7 @@
 
 #include "vshadow_test_libcstring.h"
 #include "vshadow_test_libvshadow.h"
+#include "vshadow_test_macros.h"
 #include "vshadow_test_unused.h"
 
 /* Tests retrieving the library version
@@ -46,11 +46,15 @@ int vshadow_test_get_version(
 	          LIBVSHADOW_VERSION_STRING,
 	          9 );
 
-	if( result != 0 )
-	{
-		return( 0 );
-	}
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
 	return( 1 );
+
+on_error:
+	return( 0 );
 }
 
 /* The main program
@@ -68,10 +72,13 @@ int main(
 	VSHADOW_TEST_UNREFERENCED_PARAMETER( argc )
 	VSHADOW_TEST_UNREFERENCED_PARAMETER( argv )
 
-	if( vshadow_test_get_version() != 1 )
-	{
-		return( EXIT_FAILURE );
-	}
+	VSHADOW_TEST_RUN(
+	 "libvshadow_get_version",
+	 vshadow_test_get_version() )
+
 	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
 }
 
--- tests/vshadow_test_memory.h
+++ tests/vshadow_test_memory.h
@@ -1,5 +1,5 @@
 /*
- * Notification functions
+ * Memory allocation functions for testing
  *
  * Copyright (C) 2011-2016, Joachim Metz <joachim.metz@gmail.com>
  *
@@ -19,45 +19,32 @@
  * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if !defined( _LIBVSHADOW_NOTIFY_H )
-#define _LIBVSHADOW_NOTIFY_H
+#if !defined( _VSHADOW_TEST_MEMORY_H )
+#define _VSHADOW_TEST_MEMORY_H
 
 #include <common.h>
-#include <file_stream.h>
-#include <types.h>
-
-#include "libvshadow_extern.h"
-#include "libvshadow_libcerror.h"
 
 #if defined( __cplusplus )
 extern "C" {
 #endif
 
-#if !defined( HAVE_LOCAL_LIBVSHADOW )
+#if defined( HAVE_GNU_DL_DLSYM ) && defined( __GNUC__ ) && !defined( __clang__ )
 
-LIBVSHADOW_EXTERN \
-void libvshadow_notify_set_verbose(
-      int verbose );
+#define HAVE_VSHADOW_TEST_MEMORY		1
 
-LIBVSHADOW_EXTERN \
-int libvshadow_notify_set_stream(
-     FILE *stream,
-     libcerror_error_t **error );
+extern int vshadow_test_malloc_attempts_before_fail;
 
-LIBVSHADOW_EXTERN \
-int libvshadow_notify_stream_open(
-     const char *filename,
-     libcerror_error_t **error );
+extern int vshadow_test_memcpy_attempts_before_fail;
 
-LIBVSHADOW_EXTERN \
-int libvshadow_notify_stream_close(
-     libcerror_error_t **error );
+extern int vshadow_test_memset_attempts_before_fail;
 
-#endif /* !defined( HAVE_LOCAL_LIBVSHADOW ) */
+extern int vshadow_test_realloc_attempts_before_fail;
+
+#endif /* defined( HAVE_GNU_DL_DLSYM ) && defined( __GNUC__ ) && !defined( __clang__ ) */
 
 #if defined( __cplusplus )
 }
 #endif
 
-#endif /* !defined( _LIBVSHADOW_NOTIFY_H ) */
+#endif /* !defined( _VSHADOW_TEST_MEMORY_H ) */
 
