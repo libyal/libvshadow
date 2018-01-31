54aee169a8412a2d1bed78198a61a25a61d6e39b
Joachim Metz <joachim.metz@gmail.com>
2018-01-31 23:14:42 +0100

    Applied updates

--- vshadowtools/vshadowtools_output.c
+++ vshadowtools/vshadowtools_output.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the vshadowtools
  *
- * Copyright (C) 2011-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -108,7 +108,7 @@ void vshadowoutput_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2011-2017, %s.\n" ),
+	 _( "Copyright (C) 2011-2018, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
