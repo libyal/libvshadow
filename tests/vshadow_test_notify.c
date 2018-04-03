3e58d2808093f255df13abcce70ead809b29c62a
Joachim Metz <joachim.metz@gmail.com>
2018-04-03 05:41:45 +0200

    Applied updates

--- tests/vshadow_test_notify.c
+++ tests/vshadow_test_notify.c
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #endif
 
+#include "vshadow_test_libcerror.h"
 #include "vshadow_test_libvshadow.h"
 #include "vshadow_test_macros.h"
 #include "vshadow_test_unused.h"
@@ -51,13 +52,37 @@ int vshadow_test_notify_set_verbose(
 int vshadow_test_notify_set_stream(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libvshadow_notify_set_stream(
-	 NULL,
-	 NULL );
+	result = libvshadow_notify_set_stream(
+	          NULL,
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	VSHADOW_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_set failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libvshadow_notify_stream_open function
@@ -66,13 +91,65 @@ int vshadow_test_notify_set_stream(
 int vshadow_test_notify_stream_open(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libvshadow_notify_stream_open(
-	 NULL,
-	 NULL );
+	result = libvshadow_notify_stream_open(
+	          "notify_stream.log",
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	VSHADOW_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libvshadow_notify_stream_open(
+	          NULL,
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	/* Clean up
+	 */
+	result = libvshadow_notify_stream_close(
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	VSHADOW_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libvshadow_notify_stream_close function
@@ -81,12 +158,36 @@ int vshadow_test_notify_stream_open(
 int vshadow_test_notify_stream_close(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libvshadow_notify_stream_close(
-	 NULL );
+	result = libvshadow_notify_stream_close(
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	VSHADOW_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_close failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* The main program
--- tests/vshadow_test_support.c
+++ tests/vshadow_test_support.c
@@ -36,6 +36,7 @@
 #include "vshadow_test_libcerror.h"
 #include "vshadow_test_libvshadow.h"
 #include "vshadow_test_macros.h"
+#include "vshadow_test_memory.h"
 #include "vshadow_test_unused.h"
 
 #if !defined( LIBVSHADOW_HAVE_BFIO )
@@ -209,38 +210,40 @@ int vshadow_test_check_volume_signature(
 	libcerror_error_t *error = NULL;
 	int result               = 0;
 
-	/* Initialize test
-	 */
-	result = vshadow_test_get_narrow_source(
-	          source,
-	          narrow_source,
-	          256,
-	          &error );
+	if( source != NULL )
+	{
+		/* Initialize test
+		 */
+		result = vshadow_test_get_narrow_source(
+		          source,
+		          narrow_source,
+		          256,
+		          &error );
 
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	/* Test check volume signature
-	 */
-	result = libvshadow_check_volume_signature(
-	          narrow_source,
-	          &error );
+		/* Test check volume signature
+		 */
+		result = libvshadow_check_volume_signature(
+		          narrow_source,
+		          &error );
 
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
 	/* Test error cases
 	 */
 	result = libvshadow_check_volume_signature(
@@ -259,6 +262,54 @@ int vshadow_test_check_volume_signature(
 	libcerror_error_free(
 	 &error );
 
+	result = libvshadow_check_volume_signature(
+	          "",
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	if( source != NULL )
+	{
+#if defined( HAVE_VSHADOW_TEST_MEMORY )
+
+		/* Test libvshadow_check_volume_signature with malloc failing in libbfio_file_initialize
+		 */
+		vshadow_test_malloc_attempts_before_fail = 0;
+
+		result = libvshadow_check_volume_signature(
+		          narrow_source,
+		          &error );
+
+		if( vshadow_test_malloc_attempts_before_fail != -1 )
+		{
+			vshadow_test_malloc_attempts_before_fail = -1;
+		}
+		else
+		{
+			VSHADOW_TEST_ASSERT_EQUAL_INT(
+			 "result",
+			 result,
+			 -1 );
+
+			VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+			 "error",
+			 error );
+
+			libcerror_error_free(
+			 &error );
+		}
+#endif /* defined( HAVE_VSHADOW_TEST_MEMORY ) */
+	}
 	return( 1 );
 
 on_error:
@@ -283,38 +334,40 @@ int vshadow_test_check_volume_signature_wide(
 	libcerror_error_t *error = NULL;
 	int result               = 0;
 
-	/* Initialize test
-	 */
-	result = vshadow_test_get_wide_source(
-	          source,
-	          wide_source,
-	          256,
-	          &error );
+	if( source != NULL )
+	{
+		/* Initialize test
+		 */
+		result = vshadow_test_get_wide_source(
+		          source,
+		          wide_source,
+		          256,
+		          &error );
 
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	/* Test check volume signature
-	 */
-	result = libvshadow_check_volume_signature_wide(
-	          wide_source,
-	          &error );
+		/* Test check volume signature
+		 */
+		result = libvshadow_check_volume_signature_wide(
+		          wide_source,
+		          &error );
 
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
 	/* Test error cases
 	 */
 	result = libvshadow_check_volume_signature_wide(
@@ -333,6 +386,54 @@ int vshadow_test_check_volume_signature_wide(
 	libcerror_error_free(
 	 &error );
 
+	result = libvshadow_check_volume_signature_wide(
+	          L"",
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	if( source != NULL )
+	{
+#if defined( HAVE_VSHADOW_TEST_MEMORY )
+
+		/* Test libvshadow_check_volume_signature_wide with malloc failing in libbfio_file_initialize
+		 */
+		vshadow_test_malloc_attempts_before_fail = 0;
+
+		result = libvshadow_check_volume_signature_wide(
+		          wide_source,
+		          &error );
+
+		if( vshadow_test_malloc_attempts_before_fail != -1 )
+		{
+			vshadow_test_malloc_attempts_before_fail = -1;
+		}
+		else
+		{
+			VSHADOW_TEST_ASSERT_EQUAL_INT(
+			 "result",
+			 result,
+			 -1 );
+
+			VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+			 "error",
+			 error );
+
+			libcerror_error_free(
+			 &error );
+		}
+#endif /* defined( HAVE_VSHADOW_TEST_MEMORY ) */
+	}
 	return( 1 );
 
 on_error:
@@ -362,77 +463,90 @@ int vshadow_test_check_volume_signature_file_io_handle(
 
 	/* Initialize test
 	 */
-	result = libbfio_file_initialize(
-	          &file_io_handle,
-	          &error );
-
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+	memset_result = memory_set(
+	                 empty_block,
+	                 0,
+	                 sizeof( uint8_t ) * 8192 );
 
 	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
-	 "file_io_handle",
-	 file_io_handle );
+	 "memset_result",
+	 memset_result );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+	if( source != NULL )
+	{
+		/* Initialize test
+		 */
+		result = libbfio_file_initialize(
+		          &file_io_handle,
+		          &error );
 
-	source_length = system_string_length(
-	                 source );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
+
+		VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+		 "file_io_handle",
+		 file_io_handle );
+
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+
+		source_length = system_string_length(
+		                 source );
 
 #if defined( HAVE_WIDE_SYSTEM_CHARACTER )
-	result = libbfio_file_set_name_wide(
-	          file_io_handle,
-	          source,
-	          source_length,
-	          &error );
+		result = libbfio_file_set_name_wide(
+		          file_io_handle,
+		          source,
+		          source_length,
+		          &error );
 #else
-	result = libbfio_file_set_name(
-	          file_io_handle,
-	          source,
-	          source_length,
-	          &error );
+		result = libbfio_file_set_name(
+		          file_io_handle,
+		          source,
+		          source_length,
+		          &error );
 #endif
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	result = libbfio_handle_open(
-	          file_io_handle,
-	          LIBBFIO_OPEN_READ,
-	          &error );
+		result = libbfio_handle_open(
+		          file_io_handle,
+		          LIBBFIO_OPEN_READ,
+		          &error );
 
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	/* Test check volume signature
-	 */
-	result = libvshadow_check_volume_signature_file_io_handle(
-	          file_io_handle,
-	          &error );
+		/* Test check volume signature
+		 */
+		result = libvshadow_check_volume_signature_file_io_handle(
+		          file_io_handle,
+		          &error );
 
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
 	/* Test error cases
 	 */
 	result = libvshadow_check_volume_signature_file_io_handle(
@@ -453,8 +567,77 @@ int vshadow_test_check_volume_signature_file_io_handle(
 
 	/* Clean up
 	 */
-	result = libbfio_handle_close(
+	if( source != NULL )
+	{
+		result = libbfio_handle_close(
+		          file_io_handle,
+		          &error );
+
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 0 );
+
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+
+		result = libbfio_handle_free(
+		          &file_io_handle,
+		          &error );
+
+		VSHADOW_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
+
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "file_io_handle",
+		 file_io_handle );
+
+		VSHADOW_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
+	/* Test check volume signature with data too small
+	 */
+	result = vshadow_test_open_file_io_handle(
+	          &file_io_handle,
+	          empty_block,
+	          sizeof( uint8_t ) * 1,
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+	 "file_io_handle",
+	 file_io_handle );
+
+	VSHADOW_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	result = libvshadow_check_volume_signature_file_io_handle(
 	          file_io_handle,
+	          &error );
+
+	VSHADOW_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = vshadow_test_close_file_io_handle(
+	          &file_io_handle,
 	          &error );
 
 	VSHADOW_TEST_ASSERT_EQUAL_INT(
@@ -466,53 +649,10 @@ int vshadow_test_check_volume_signature_file_io_handle(
 	 "error",
 	 error );
 
-	result = libbfio_handle_free(
-	          &file_io_handle,
-	          &error );
-
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
-
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "file_io_handle",
-	 file_io_handle );
-
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	/* Initialize test
+	/* Test check volume signature with empty block
 	 */
-	memset_result = memory_set(
-	                 empty_block,
-	                 0,
-	                 sizeof( uint8_t ) * 8192 );
-
-	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
-	 "memset_result",
-	 memset_result );
-
-	result = libbfio_memory_range_initialize(
+	result = vshadow_test_open_file_io_handle(
 	          &file_io_handle,
-	          &error );
-
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
-
-	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
-	 "file_io_handle",
-	 file_io_handle );
-
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	result = libbfio_memory_range_set(
-	          file_io_handle,
 	          empty_block,
 	          sizeof( uint8_t ) * 8192,
 	          &error );
@@ -522,26 +662,14 @@ int vshadow_test_check_volume_signature_file_io_handle(
 	 result,
 	 1 );
 
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	result = libbfio_handle_open(
-	          file_io_handle,
-	          LIBBFIO_OPEN_READ,
-	          &error );
-
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+	VSHADOW_TEST_ASSERT_IS_NOT_NULL(
+	 "file_io_handle",
+	 file_io_handle );
 
 	VSHADOW_TEST_ASSERT_IS_NULL(
 	 "error",
 	 error );
 
-	/* Test check volume signature
-	 */
 	result = libvshadow_check_volume_signature_file_io_handle(
 	          file_io_handle,
 	          &error );
@@ -555,10 +683,8 @@ int vshadow_test_check_volume_signature_file_io_handle(
 	 "error",
 	 error );
 
-	/* Clean up
-	 */
-	result = libbfio_handle_close(
-	          file_io_handle,
+	result = vshadow_test_close_file_io_handle(
+	          &file_io_handle,
 	          &error );
 
 	VSHADOW_TEST_ASSERT_EQUAL_INT(
@@ -569,25 +695,6 @@ int vshadow_test_check_volume_signature_file_io_handle(
 	VSHADOW_TEST_ASSERT_IS_NULL(
 	 "error",
 	 error );
-
-	result = libbfio_handle_free(
-	          &file_io_handle,
-	          &error );
-
-	VSHADOW_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
-
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "file_io_handle",
-	 file_io_handle );
-
-	VSHADOW_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	/* TODO test volume too small */
 
 	return( 1 );
 
