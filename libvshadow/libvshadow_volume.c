74330c8a402b060b72a3e7d0d3b4840cac1e712d
Joachim Metz <joachim.metz@gmail.com>
2023-08-12 05:57:14 +0200

    Applied updates

--- libvshadow/libvshadow_volume.c
+++ libvshadow/libvshadow_volume.c
@@ -641,9 +641,9 @@ int libvshadow_volume_open_file_io_handle(
 {
 	libvshadow_internal_volume_t *internal_volume = NULL;
 	static char *function                         = "libvshadow_volume_open_file_io_handle";
+	uint8_t file_io_handle_opened_in_library      = 0;
 	int bfio_access_flags                         = 0;
 	int file_io_handle_is_open                    = 0;
-	int file_io_handle_opened_in_library          = 0;
 
 	if( volume == NULL )
 	{
