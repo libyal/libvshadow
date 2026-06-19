eb5590eb1c922a3b107a4af319f34382dac5ba71
Joachim Metz <joachim.metz@gmail.com>
2026-06-19 14:15:34 +0200

    Worked on tests and changes for CI tests

--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,71 +1,10 @@
 # Tests library functions and types.
-#
-# Version: 20260615
 
 $LibraryTests = "block block_descriptor block_range_descriptor block_tree block_tree_node error io_handle notify store_block store_descriptor"
 $LibraryTestsWithInput = "store support volume"
 $OptionSets = "offset" -split " "
 
 . .\test_functions.ps1
-
-Function RunTest
-{
-	param( [string]$TestName )
-
-	$TestBinary = "vshadow_test_${TestName}"
-
-	$TestDescription = "${TestBinary}"
-	$TestExecutable = "${TestExecutablesDirectory}\${TestBinary}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		WriteTestResult ${TestDescription} ${ExitIgnore}
-
-		Return ${ExitIgnore}
-	}
-	$Output = Invoke-Expression ${TestExecutable}
-	$Result = $global:LastExitCode
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	WriteTestResult ${TestDescription} ${Result}
-
-	Return ${Result}
-}
-
-Function RunTestWithInput
-{
-	param( [string]$TestName, [string[]]$TestInput )
-
-	$OptionSet = $TestInput[0]
-	$Options = $TestInput[1]
-	$TestFile = $TestInput[2]
-
-	$TestBinary = "vshadow_test_${TestName}"
-	$TestFileName = (${TestFile} -split '\\')[-2..-1] -join '\'
-
-	$TestDescription = "${TestBinary} with input: '${TestFileName}"
-	$TestExecutable = "${TestExecutablesDirectory}\${TestBinary}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		WriteTestResult ${TestDescription} ${ExitIgnore}
-
-		Return ${ExitIgnore}
-	}
-	$Output = Invoke-Expression "${TestExecutable} ${Options} ${TestFile}"
-	$Result = $global:LastExitCode
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	WriteTestResult ${TestDescription} ${Result}
-
-	Return ${Result}
-}
 
 $TestExecutablesDirectory = GetTestExecutablesDirectory
 
@@ -85,7 +24,7 @@ Foreach (${TestName} in ${LibraryTests} -split " ")
 	{
 		Continue
 	}
-	$Result = RunTest ${TestName}
+	$Result = RunTestBinary ${TestExecutablesDirectory} "vshadow_test_${TestName}"
 
 	If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 	{
@@ -104,7 +43,7 @@ Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 	}
 	ForEach ($TestInput in ${TestInputs})
 	{
-		$Result = RunTestWithInput ${TestName} ${TestInput}
+		$Result = RunTestBinaryWithInput ${TestExecutablesDirectory} "vshadow_test_${TestName}" ${TestInput}
 
 		If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 		{
---
+++ tests/test_tools.ps1
@@ -0,0 +1,125 @@
+# Tests tools functions and types.
+
+$ToolsTests = ""
+$OptionSets = "offset" -split " "
+
+. .\test_functions.ps1
+
+Function CompareWithReference
+{
+	param( [string]$TestProfileDirectory, [string]$TestSet, [string]$TestFileName, [string]$TestResults )
+
+	$ExpectedTestResults = "${TestProfileDirectory}\${TestSet}\${TestFileName}"
+
+	If (Test-Path -Path ${ExpectedTestResults} -PathType Leaf)
+	{
+		$Difference = Compare-Object -ReferenceObject (Get-Content -Path ${ExpectedTestResults}) -DifferenceObject (Get-Content -Path ${TestResults})
+
+		If (${Difference})
+		{
+			Return ${ExitFailure}
+		}
+	}
+	Else
+	{
+		New-Item -Force -ItemType Directory -Path "${TestProfileDirectory}\${TestSet}" | Out-Null
+		Move-Item -Path ${TestResults} -Destination ${ExpectedTestResults}
+	}
+	Return ${ExitSuccess}
+}
+
+Function RunToolsBinaryAndCompareStdout
+{
+	param( [string]$TestExecutablesDirectory, [string]$ToolName, [string]$TestProfile, [string]$TestOptions, [string[]]$TestInput )
+
+	$TestExecutable = "${TestExecutablesDirectory}\${ToolName}.exe"
+
+	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
+	{
+		$TestDescription = "Missing binary: ${ToolName}"
+		WriteTestResult ${TestDescription} ${ExitCommandNotFound}
+
+		Return ${ExitCommandNotFound}
+	}
+	$OptionSet = $TestInput[0]
+	$Options = $TestInput[1]
+	$TestFile = $TestInput[2]
+
+	$TestProfileDirectory = "input\.${TestProfile}"
+	$TestSet = Split-Path (Split-Path -Path ${TestFile} -Parent) -Leaf
+	$TestFileName = Split-Path -Path ${TestFile} -Leaf
+
+	If ($OptionSet)
+	{
+		$OutputFile = "${TestFileName}-${OptionSet}.log"
+	}
+	Else
+	{
+		$OutputFile = "${TestFileName}.log"
+	}
+	$TmpDir = "tmp${PID}"
+
+	New-Item -Name ${TmpDir} -ItemType "directory" | Out-Null
+
+	Push-Location ${TmpDir}
+
+	Try
+	{
+		Invoke-Expression "..\${TestExecutable} ${Options} ${TestFile} > ${OutputFile}"
+		$Result = $global:LastExitCode
+
+		If (${Result} -eq ${ExitSuccess})
+		{
+			# Strip header with version.
+			(Get-Content ${OutputFile} | Select-Object -Skip 2) | Set-Content ${OutputFile}
+
+			$Result = CompareWithReference "..\${TestProfileDirectory}" ${TestSet} ${TestFileName} ${OutputFile}
+		}
+	}
+	Finally
+	{
+		Pop-Location
+
+		Remove-Item ${TmpDir} -Force -Recurse
+	}
+	$TestDescription = "${ToolName} with input: '${TestSet}\${TestFileName}"
+	WriteTestResult ${TestDescription} ${Result}
+
+	Return ${Result}
+}
+
+$TestExecutablesDirectory = GetTestExecutablesDirectory
+
+If (-Not (Test-Path ${TestExecutablesDirectory}))
+{
+	Write-Error "Missing test executables directory"
+
+	Exit ${ExitFailure}
+}
+
+$Result = ${ExitIgnore}
+
+$Profiles = @("vshadowinfo")
+
+For ($ProfileIndex = 0; $ProfileIndex -le ($Profiles.length - 1); $ProfileIndex += 1)
+{
+	$TestProfile = $Profiles[$ProfileIndex]
+
+	$TestInputs = GenerateTestInputs ${TestProfile} ${OptionSets}
+
+	ForEach ($TestInput in ${TestInputs})
+	{
+		$Result = RunToolsBinaryAndCompareStdout ${TestExecutablesDirectory} "vshadowinfo" ${TestProfile} "" ${TestInput}
+
+		If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
+		{
+			Break
+		}
+	}
+	If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
+	{
+		Break
+	}
+}
+
+Exit ${Result}
